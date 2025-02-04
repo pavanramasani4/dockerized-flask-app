40a2b6271b4f44e93ca285433296c45c28573ec3
pavanramasani4 <139073956+pavanramasani4@users.noreply.github.com>
2025-02-03 20:21:26 -0600

    Add files via upload

---
+++ tests/test_Web.py
@@ -0,0 +1,15 @@
+import unittest
+from WebPage.Week1 import app
+
+class FlaskAppTests(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Welcome', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
